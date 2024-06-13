3eeb3c936e70bffdc6b43cb14420c6fee46d4394
GhugareSanika <144719999+GhugareSanika@users.noreply.github.com>
2024-06-13 23:37:26 +0530

    Update Backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,31 +1,74 @@
 "use strict";
+// import express, { Request, Response } from "express";
+// import http from "http";
+// import { Server, Socket } from "socket.io";
 Object.defineProperty(exports, "__esModule", { value: true });
+// const app = express();
+// const server = http.createServer(app);
+// const io = new Server(server);
+// // Routes
+// app.get("/", (req: Request, res: Response) => {
+//     res.send("This is the MERN Realtime WhiteBoard sharing app");
+// });
+// let roomIdGlobal: string | undefined;
+// let imgURLGlobal: string | undefined;
+// io.on("connection", (socket: Socket) => {
+//     socket.on("userJoined", (data: { name: string; userId: string; roomId: string; host: boolean; presenter: boolean }) => {
+//         const {name,userId,roomId, host, presenter } = data;
+//         roomIdGlobal = roomId;
+//         socket.join(roomId);
+//         socket.emit("userIsJoined", { success: true });
+//         socket.broadcast.to(roomId).emit("whiteBoardDataResponse", {
+//             imgURL: imgURLGlobal,
+//         });
+//     });
+//     socket.on("whiteBoardDataResponse", (data: string) => {
+//         imgURLGlobal = data;
+//         if (roomIdGlobal) {
+//             socket.broadcast.to(roomIdGlobal).emit("whiteBoardDataResponse", {
+//                 imgURL: data,
+//             });
+//         }
+//     });
+// });
+// const port = process.env.PORT || 5000;
+// server.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
 var express_1 = require("express");
 var http_1 = require("http");
 var socket_io_1 = require("socket.io");
 var app = (0, express_1.default)();
 var server = http_1.default.createServer(app);
-var io = new socket_io_1.Server(server);
+//const io = new Server(server);
+var io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
 // Routes
 app.get("/", function (req, res) {
     res.send("This is the MERN Realtime WhiteBoard sharing app");
 });
-var roomIdGlobal, imgURLGlobal;
+// Store image URLs for each room
+var roomImages = new Map();
 io.on("connection", function (socket) {
     socket.on("userJoined", function (data) {
         var name = data.name, userId = data.userId, roomId = data.roomId, host = data.host, presenter = data.presenter;
-        roomIdGlobal = roomId;
         socket.join(roomId);
         socket.emit("userIsJoined", { success: true });
-        socket.broadcast.to(roomId).emit("whiteBoardDataResponse", {
-            imgURL: imgURLGlobal,
-        });
+        // Send the latest whiteboard image to the newly joined user
+        var imgURL = roomImages.get(roomId);
+        socket.emit("whiteBoardDataResponse", { imgURL: imgURL });
+        socket.broadcast.to(roomId).emit("userJoined", { name: name, userId: userId, host: host, presenter: presenter });
     });
     socket.on("whiteBoardDataResponse", function (data) {
-        imgURLGlobal = data;
-        socket.broadcast.to(roomIdGlobal).emit("whiteBoardDataResponse", {
-            imgURL: data,
-        });
+        var roomId = Array.from(socket.rooms).find(function (room) { return room !== socket.id; });
+        if (roomId) {
+            // Update the image URL for the specific room
+            roomImages.set(roomId, data);
+            // Broadcast the new image URL to all other users in the room
+            socket.broadcast.to(roomId).emit("whiteBoardDataResponse", { imgURL: data });
+        }
     });
 });
 var port = process.env.PORT || 5000;
--- Backend/server.ts
+++ Backend/server.ts
@@ -1,3 +1,45 @@
+// import express, { Request, Response } from "express";
+// import http from "http";
+// import { Server, Socket } from "socket.io";
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = new Server(server);
+
+// // Routes
+// app.get("/", (req: Request, res: Response) => {
+//     res.send("This is the MERN Realtime WhiteBoard sharing app");
+// });
+
+// let roomIdGlobal: string | undefined;
+// let imgURLGlobal: string | undefined;
+
+// io.on("connection", (socket: Socket) => {
+//     socket.on("userJoined", (data: { name: string; userId: string; roomId: string; host: boolean; presenter: boolean }) => {
+//         const {name,userId,roomId, host, presenter } = data;
+//         roomIdGlobal = roomId;
+//         socket.join(roomId);
+//         socket.emit("userIsJoined", { success: true });
+//         socket.broadcast.to(roomId).emit("whiteBoardDataResponse", {
+//             imgURL: imgURLGlobal,
+//         });
+//     });
+
+//     socket.on("whiteBoardDataResponse", (data: string) => {
+//         imgURLGlobal = data;
+//         if (roomIdGlobal) {
+//             socket.broadcast.to(roomIdGlobal).emit("whiteBoardDataResponse", {
+//                 imgURL: data,
+//             });
+//         }
+//     });
+// });
+
+// const port = process.env.PORT || 5000;
+
+// server.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
+
+
 import express, { Request, Response } from "express";
 import http from "http";
 import { Server, Socket } from "socket.io";
@@ -6,30 +48,43 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// const io= new Server(server,{
+//     cors:{
+//         origin:"*",
+//         methods:["GET","POST"],
+//     },
+// })
+
 // Routes
 app.get("/", (req: Request, res: Response) => {
     res.send("This is the MERN Realtime WhiteBoard sharing app");
 });
 
-let roomIdGlobal, imgURLGlobal;
+// Store image URLs for each room
+const roomImages: Map<string, string | undefined> = new Map();
 
 io.on("connection", (socket: Socket) => {
-    socket.on("userJoined", (data) =>{
+    socket.on("userJoined", (data: { name: string; userId: string; roomId: string; host: boolean; presenter: boolean }) => {
         const { name, userId, roomId, host, presenter } = data;
-        roomIdGlobal = roomId;
         socket.join(roomId);
-        socket.emit("userIsJoined",{ success: true});
-        socket.broadcast.to(roomId).emit("whiteBoardDataResponse",{
-            imgURL : imgURLGlobal,
-        })
-    })
+        socket.emit("userIsJoined", { success: true });
 
-    socket.on("whiteBoardDataResponse",(data)=>{
-        imgURLGlobal = data;
-        socket.broadcast.to(roomIdGlobal).emit("whiteBoardDataResponse",{
-        imgURL : data,
-        })
-    })
+        // Send the latest whiteboard image to the newly joined user
+        const imgURL = roomImages.get(roomId);
+        socket.emit("whiteBoardDataResponse", { imgURL });
+
+        socket.broadcast.to(roomId).emit("userJoined", { name, userId, host, presenter });
+    });
+
+    socket.on("whiteBoardDataResponse", (data: string) => {
+        const roomId = Array.from(socket.rooms).find(room => room !== socket.id);
+        if (roomId) {
+            // Update the image URL for the specific room
+            roomImages.set(roomId, data);
+            // Broadcast the new image URL to all other users in the room
+            socket.broadcast.to(roomId).emit("whiteBoardDataResponse", { imgURL: data });
+        }
+    });
 });
 
 const port = process.env.PORT || 5000;
