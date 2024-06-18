d8728f7f6767d3471e35de1430c940be255bc15e
GhugareSanika <144719999+GhugareSanika@users.noreply.github.com>
2024-06-19 00:25:21 +0530

    Update Frontend

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // import * as React from 'react';
 // import ReactDOM from 'react-dom/client';
 // import App from './App';
@@ -18,43 +18,27 @@
 //   document.getElementById("root")
 // );
 
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
->>>>>>> origin/main
+import ReactDOM from 'react-dom/client'; // Use the correct import for createRoot
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import { BrowserRouter as Router } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.min.css';
 
-<<<<<<< HEAD
-// Assuming you are using ReactDOM from 'react-dom/client'
-=======
-// Assuming you are using ReactDOM from 'react-dom'
->>>>>>> origin/main
 const rootElement = document.getElementById('root');
 
 // Check if the root element exists before rendering
 if (rootElement) {
-<<<<<<< HEAD
   const root = ReactDOM.createRoot(rootElement);
   root.render(
-=======
-  ReactDOM.render(
->>>>>>> origin/main
     <React.StrictMode>
       <Router>
         <App />
       </Router>
-<<<<<<< HEAD
     </React.StrictMode>
-=======
-    </React.StrictMode>,
-    rootElement
->>>>>>> origin/main
   );
 }
